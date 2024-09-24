ca8ae231d752ec907d0fe1fe6c77ce7b9030d26a
zxuu0105 <zxuu0105@student.monash.edu>
2024-09-24 20:11:24 +1000

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,8 +1,8 @@
-// 引用外部的 Vega-Lite JSON 文件
+
 var vg_1 = "visualization.vl.json";
 
-// 使用 vegaEmbed 函数将 JSON 文件嵌入到指定的 div 中
+
 vegaEmbed("#state_map", vg_1).then(function(result) {
-  // 可选：访问 Vega 的视图实例进行进一步的交互
+
   console.log(result.view);
 }).catch(console.error);
